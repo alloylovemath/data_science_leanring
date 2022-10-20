249373cb98ca64ef66f23cca28e25d6f40cb0376
alloylovemath <102270726+alloylovemath@users.noreply.github.com>
2022-10-21 03:00:14 +0800

    Delete 3_regression.r

--- statistics/3_regression/3_regression.r
+++
@@ -1,6 +0,0 @@
-# <Simple Linear Regression>
-
-# 1. case 1
-data_1 = read.csv("D:/daily_learning/data_science/statistics/sta/al_to.csv")
-model = lm(formula= Alcohol ~ Tobacco, data=data_1[1:10,])
-summary(model)
